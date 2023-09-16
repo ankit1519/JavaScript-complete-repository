26980f2977504e0283a52f5c14055d8bdf654bd0
Ankit Raj <90920002+ankit1519@users.noreply.github.com>
2023-09-16 19:27:08 +0000

    learnt variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=123456;
+let email="ankit@gmail.com";
+var pas="12345";
+accountCity="Patna";
+//accountId=2;
+let accountState;
+console.log(accountId);
+console.table([accountId,email,pas,accountCity,accountState]);
+/*
+
+       PREFER NOT TO USE VAR BCZ OF ISSUE IN
+       BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
