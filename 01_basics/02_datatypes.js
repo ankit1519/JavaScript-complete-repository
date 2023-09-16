31b7b96ac777297a8687f05750ec49c6767de597
Ankit Raj <90920002+ankit1519@users.noreply.github.com>
2023-09-16 20:51:17 +0000

    datatypes concept

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+//alert(3+3) //we r using nodejs, not browser
+
+
+//maintain code readiability
+
+
+//data types
+let name="Ankit"
+let age=22;
+let isIn=true
+
+// -------Primitive data types-------------
+//1. Number - 2^52
+//2. bigInt
+// 3. String => ""
+//4. boolean =>true, false
+//5. null - standalone value
+//6. undefined 
+// 7. symbol => for unique
+
+//object
+console.log(typeof age);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
