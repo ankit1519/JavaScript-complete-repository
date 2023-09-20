37cd0399e8ac948d9ce5d2c160f5b3801fb45336
Ankit Raj <90920002+ankit1519@users.noreply.github.com>
2023-09-20 21:03:48 +0000

    while and do while loop done

---
+++ 05_iterations/02_whileLoopAndDoWhileLoop.js
@@ -0,0 +1,20 @@
+let i=0
+while (i<=10) {
+    console.log(`Value of Index is ${i}`);
+    i++;
+}
+
+let arr=0;
+let myArr=["ankit","Raj",10,true,false,null]
+while(arr<myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr++
+}
+
+//do while
+//let score=1
+let score=11
+do{
+    console.log(score);
+    score++
+}while(score<=10)
